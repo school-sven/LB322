8e2cfdf1ac3bb4836bad3e0e04fe34d87f0c971c
Sven Märki <95250617+svenmaerki@users.noreply.github.com>
2022-10-27 20:59:21 +0200

    resolved error

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,42 @@
 import { TestBed } from '@angular/core/testing';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ModalModule, BsModalService } from 'ngx-bootstrap/modal';
+
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './services/routing/app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppointmentComponent } from './home/appointment/appointment.component';
+import { AppointmentTableComponent } from './home/appointment-table/appointment-table.component';
+import { PositionComponent } from './position/position.component';
+import { OpeningHoursComponent } from './opening-hours/opening-hours.component';
+import { ToastrModule } from 'ngx-toastr';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent,
+        NavbarComponent,
+        LoginComponent,
+        AppointmentComponent,
+        AppointmentTableComponent,
+        PositionComponent,
+        OpeningHoursComponent,
       ],
+      imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        ModalModule,
+        BrowserAnimationsModule,
+        ToastrModule.forRoot(),
+      ],
+      providers: [BsModalService],
     }).compileComponents();
   });
 
@@ -20,5 +50,12 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('LB322');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('LB322 app is running!');
   });
 });

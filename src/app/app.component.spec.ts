3e14c0ce3047c1c3a67e99e5f695335c92da1875
Sven Märki <95250617+svenmaerki@users.noreply.github.com>
2022-10-27 21:02:35 +0200

    removed test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -51,11 +51,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('LB322');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('LB322 app is running!');
-  });
 });
